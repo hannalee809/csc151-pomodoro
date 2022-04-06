a61a5f7467dadc7d7df2c481206876468b753bc9
Hanna Lee <hannalee809@gmail.com>
2022-04-06 14:58:33 -0400

    Added timer.rb file

---
+++ timer.rb
@@ -0,0 +1,23 @@
+# # Starter file for timer app
+# require 'ruby2d'
+
+# # Set window title and size
+# set title: "Pomodoro Timer"
+# set width: 800
+# set height: 600
+
+# timer class
+class Timer
+    
+    # start timer
+    def timer_start(len)
+        
+    end
+
+    # stop timer
+
+    # save timer length for when it is the 'pomodoro'
+
+end
+# Load and position timer screen
+
