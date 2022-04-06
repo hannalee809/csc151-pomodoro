53de2bdc30a3a274925d0c935f0847067d95ddc3
Hanna Lee <hannalee809@gmail.com>
2022-04-06 16:45:47 -0400

    Added more timer adjustments so that the timer countdown is able to show in the console

--- timer.rb
+++ timer.rb
@@ -8,6 +8,7 @@ set height: 600
 set background: '#F4EAE6'
 
 timer_num = Time.new(0)
+timer_len = 0
 
 # draw out timer elements
 Rectangle.new(x: 200, y: 125, width: 400, height: 300, color: '#E57F84')
@@ -22,12 +23,13 @@ timer_clock = Text.new("#{timer_num}", x: 320, y: 200, size: 75)
 class Timer
     
     # start timer
-    def timer_start
-        countdown_time_in_seconds = 1500
-
+    def timer_start(len)
+        t = Time.new(0)
+        countdown_time_in_seconds = len # pomodoro len = 1500, short break = 300, long break = 900
+        
         countdown_time_in_seconds.downto(0) do |seconds|
-            puts (timer_num + seconds).strftime('%M:%S')
-
+          timer_num =  (t + seconds).strftime('%M:%S')
+          sleep 1
         end
     
     end
@@ -41,23 +43,35 @@ class Timer
 
 end
 # Load and position timer screen
+update do 
+    timer_clock = Text.new("#{timer_num}", x: 320, y: 200, size: 75)
+end
 
 on :mouse_down do |event|
 
     # if pomodoro text clicked
     if pom_text.contains? event.x, event.y
        puts "Pomodoro" 
-        Timer.timer_start
+
+       timer_len = 1500
     end
 
     # if short break clicked
     if short_text.contains? event.x, event.y
         puts "Short Break"
+        #Timer.new.timer_start(len = )
+        timer_len = 300
     end
 
     # if long break clicked
     if long_text.contains? event.x, event.y
         puts "Long break"
+        timer_len = 900
+    end
+
+    if start_text.contains? event.x, event.y
+        puts timer_len
+        Timer.new.timer_start(len = timer_len)
     end
 
 end
