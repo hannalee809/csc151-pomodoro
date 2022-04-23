101473b046972705c28ae89e79c5292151473d2b
Hanna Lee <hannalee809@gmail.com>
2022-04-23 12:03:14 -0400

    Finished settings function

--- timer.rb
+++ timer.rb
@@ -60,62 +60,6 @@ long_text = Text.new('Long Break', x: 475, y: 130, size: 20)
 timer_clock = Text.new("#{timer_num}", x: 260, y: 200, size: 75)
 settings_text = Text.new('Settings', x: 700, y: 10, size: 20, color: '#4297A0')
 
-def settings(pom_num, long_break, short_break)
-    # set title: "Settings"
-    # set width: 800
-    # set height: 600
-    # set background: '#F4EAE6'
-    
-    # draw out settings' elements
-    settings_box = Rectangle.new(x: 10, y: 125, width: 180, height: 300, color: '#4297A0')
-    time_text = Text.new('Time (minutes)', x: 15, y: 130, size: 25)
-    exit_button = Rectangle.new(x: 175, y: 125, width: 15, height: 15, color: 'red')
-    exit_text = Text.new('X', x: 175, y: 125, size: 15)
-
-    pom_text = Text.new('Pomodoro:', x: 15, y: 185, size: 15)
-    short_text = Text.new('Short Break:', x: 15, y: 235, size: 15)
-    long_text = Text.new('Long Break:', x: 15, y: 285, size: 15)
-
-    alarm_text = Text.new('Alarm Sound', x: 15, y: 335, size: 25)
-
-    puts 'Enter an amount of minutes for Pomodoro timer:'
-    pom_num = gets.chomp.to_i
-
-    puts 'Enter an amount of minutes for short break:'
-    short_break = gets.chomp.to_i
-
-    puts 'Enter an amoung of minutes for long break:'
-    long_break = gets.chomp.to_i
-
-    settings_on_off = false
-    
-    #  on :mouse_down do |event|
-    #     if pom_text.contains?(event.x, event.y)
-    #         puts 'Enter an amount of minutes for Pomodoro timer:'
-    #         pom_num = gets.chomp.to_i
-    #         return pom_num
-    #     end
-
-    #     if short_text.contains?(event.x, event.y)
-    #         puts 'Enter an amount of minutes for short break:'
-    #         short_break = gets.chomp.to_i
-    #         return short_break
-    #     end
-
-    #     if long_text.contains?(event.x, event.y)
-    #         puts 'Enter an amoung of minutes for long break:'
-    #         long_break = gets.chomp.to_i
-    #         return long_break
-    #     end
-
-    #     if alarm_text.contains?(event.x, event.y)
-    #         puts 'Enter an alarm sound from this list:...'
-    #         alarm_sound = gets
-    #         alarm_sound = alarm_sound.chomp
-    #     end
-    # end
-
-end
 
 def seconds_to_hms(sec)
     "%02d:%02d:%02d" % [sec / 3600, sec / 60 % 60, sec % 60]
@@ -128,6 +72,7 @@ on :mouse_down do |event|
        puts "Pomodoro" 
        timer_len = 1500
        pomodoro_on_off = true
+       timer_clock.text = seconds_to_hms(1500)
     end
 
     # if short break clicked - len is 300
@@ -135,12 +80,16 @@ on :mouse_down do |event|
         puts "Short Break"
         #Timer.new.timer_start(len = 300)
         timer_len = 300
+        timer_clock.text = seconds_to_hms(300)
+
     end
 
     # if long break clicked - len is 900
     if long_text.contains? event.x, event.y
         puts "Long break"
         timer_len = 900
+        timer_clock.text = seconds_to_hms(900)
+
     end
 
     if start_text.contains? event.x, event.y
@@ -151,6 +100,7 @@ on :mouse_down do |event|
     if stop_text.contains? event.x, event.y
         stop_timer = true
         timer_on_off = false
+        csv_array.pop
     end
 
     if settings_text.contains? event.x, event.y
@@ -164,11 +114,9 @@ end
 tick = 0
 update do
     if timer_on_off == true
-        puts 'start has been pressed'
 
         # creates the time instance for the csv file
         csv_time = Time.now
-        puts csv_time
         
 
         # for the countdown
@@ -187,25 +135,40 @@ update do
         # stores the pomodoro time and the date into the csv array - will be converted to a csv file soon
         if pomodoro_on_off == true
             csv_pomodoro = timer_len
-            puts "csv_pomodoro is #{csv_pomodoro}"
+            # puts "csv_pomodoro is #{csv_pomodoro}"
             csv_array << [csv_time, csv_pomodoro]
-            puts csv_array
+            # puts csv_array
             csv_check("pomodoro.csv", csv_array)
             csv_array.pop
-
-        else
-            puts "because this was a short/break, this will not be recorded"
+            pomodoro_on_off = false
         end
       
     end
 
+    # if settings text is clicked, the user can choose to change the time for pomodoro, short break, or long break
     if settings_on_off == true
-        settings(15, 15, 15) 
-        break if settings_on_off == false
-        puts pom_num
-        puts short_break
-        puts long_break
+        print 'Would you like to change the length of the Pomodoro timer, short break, or long break? ' 
+        input = gets.chomp
+
+        if input == "Pomodoro"
+            print "Enter an amount of minutes for Pomodoro timer: "
+            timer_len = gets.chomp.to_i
+            pomodoro_on_off = true
+        end
+
+        if input == "Short break"
+            print "Enter the amount of minutes for Short break: "
+            timer_len = gets.chomp.to_i
+        end
+
+        if input == "Long break"
+            print "Enter the amount of minutes for Long break: "
+            timer_len = gets.chomp.to_i
+        end
+        timer_on_off = true
+        settings_on_off = false
     end
+        
 end
 
 show
